e702a7cb0d74855d90ddde391f88c7dac71b2e64
joao <j.batistareis@gmail.com>
2018-05-31 07:08:22 -0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { PhotoLibrary } from '@ionic-native/photo-library';
+import { Toast } from '@ionic-native/toast';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { AndroidPermissions } from '@ionic-native/android-permissions';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ListPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ListPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+	PhotoLibrary,
+	Toast,
+	InAppBrowser,
+	AndroidPermissions
+  ]
+})
+export class AppModule {}
